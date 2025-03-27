036cd20815befbd2185340410ca9cd471e88e74d
dmhd6219 <svyatoslavsvyatkin@yandex.ru>
2025-03-27 13:10:15 +0800

    init: add react router

---
+++ src/config/routes.tsx
@@ -0,0 +1,9 @@
+import { RouteObject } from 'react-router-dom';
+import App from '@/App.tsx';
+
+export const routesConfig: RouteObject[] = [
+    {
+        path: '/',
+        element: <App />,
+    },
+];
