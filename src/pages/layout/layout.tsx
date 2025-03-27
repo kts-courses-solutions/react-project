05f0ec3f0e2be2b927d3621f87996d23ee5779d7
dmhd6219 <svyatoslavsvyatkin@yandex.ru>
2025-03-27 18:39:31 +0800

    fix: migrate from @import to @use in css

--- src/pages/layout/layout.tsx
+++ src/pages/layout/layout.tsx
@@ -1,15 +1,16 @@
 import { Header } from '@/pages/layout';
 import { Outlet } from 'react-router-dom';
 import { layoutStyles } from '@/pages/layout';
+import { globalStyles } from '@/styles';
 
 const Layout = () => {
     return (
-        <>
+        <div className={globalStyles.layout}>
             <Header />
             <main className={layoutStyles.main}>
                 <Outlet />
             </main>
-        </>
+        </div>
     );
 };
 
