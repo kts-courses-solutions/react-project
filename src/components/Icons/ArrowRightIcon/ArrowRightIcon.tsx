36be6bfee5d3f381a4e5e4372263a3e11f7d0580
dmhd6219 <svyatoslavsvyatkin@yandex.ru>
2025-04-06 21:08:38 +0800

    fix: icons svg props names

--- src/components/Icons/ArrowRightIcon/ArrowRightIcon.tsx
+++ src/components/Icons/ArrowRightIcon/ArrowRightIcon.tsx
@@ -21,10 +21,10 @@ const ArrowRightIcon: React.FC<IconProps> = ({
         >
             <path
                 d="M22.0062 5.95005L12.4979 15.4584C11.375 16.5813 11.375 18.4188 12.4979 19.5417L22.0062 29.05"
-                stroke-width="1.5"
-                stroke-miterlimit="10"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeWidth="1.5"
+                strokeMiterlimit="10"
+                strokeLinecap="round"
+                strokeLinejoin="round"
             />
         </Icon>
     );
